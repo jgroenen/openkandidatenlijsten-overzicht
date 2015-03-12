3985bfc915c8ced16ccfc42425d9971792549391
Johan Groenen <johan.groenen+github@gmail.com>
2015-03-12 18:19:17 +0000

    mini app - stuck here because of cross origin

--- public/assets/js/app/Views/Application.js
+++ public/assets/js/app/Views/Application.js
@@ -1,13 +1,15 @@
 define([
     'settings',
-    'text!templates/application.tmpl'
-], function (settings, template) {
+    'text!templates/application.tmpl',
+    'text!/overzicht.json'
+], function (settings, template, lijsten) {
     return Backbone.View.extend({
         template: _.template(template),
 
         events: {},
 
         initialize: function () {
+            this.lijsten = JSON.parse(lijsten);
             this.render();
         },
         
